59702d4dcc9f83fc63a4f0d14fb6f25575c15720
Nguyen Tan Minh <nt.minh.2409@gmail.com>
2026-03-17 01:56:33 +0800

    Monthly View

--- lib/stats.dart
+++ lib/stats.dart
@@ -13,9 +13,47 @@ class _MyStatsPageState extends State<MyStatsPage> {
   final DatabaseHelper dbHelper = DatabaseHelper();
   Offset _tapPosition = Offset.zero;
 
+  DateTime _viewingDate = DateTime.now();
+
   void _getTabPosition(TapDownDetails details) {
     setState(() {
       _tapPosition = details.globalPosition;
+    });
+  }
+
+  void _nextYear(){
+    setState((){
+      _viewingDate = DateTime(_viewingDate.year + 1);
+    });
+  }
+
+  void _previousYear(){
+    setState((){
+      _viewingDate = DateTime(_viewingDate.year - 1);
+    });
+  }
+
+  void _nextMonth(){
+    setState((){
+      _viewingDate = DateTime(_viewingDate.year, _viewingDate.month + 1);
+    });
+  }
+
+  void _previousMonth(){
+    setState((){
+      _viewingDate = DateTime(_viewingDate.year, _viewingDate.month - 1);
+    });
+  }
+
+  void _nextDay(){
+    setState((){
+      _viewingDate = DateTime(_viewingDate.year, _viewingDate.month, _viewingDate.day + 1);
+    });
+  }
+
+  void _previousDay(){
+    setState((){
+      _viewingDate = DateTime(_viewingDate.year, _viewingDate.month, _viewingDate.day - 1);
     });
   }
 
@@ -55,73 +93,142 @@ class _MyStatsPageState extends State<MyStatsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: FutureBuilder<List<TransactionModel>>(
-        future: dbHelper.getTransactions(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return _buildEmptyState();
-          }
-
-          final transactions = snapshot.data!;
-          return ListView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 50.0),
-            itemCount: transactions.length,
-            itemBuilder: (context, index) {
-              final item = transactions[index];
-              return GestureDetector(
-                onTapDown: _getTabPosition,
-                onLongPress: () => _showContextMenu(context, item.id!),
-                onDoubleTap: () => _showContextMenu(context, item.id!),
-                onTap: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PurchaseInfo(transaction: item),
+      body: SafeArea(
+        child:
+          Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(15, 5, 15, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: (){
+                      _previousMonth();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[100],
                     ),
-                  );
-
-                  if(result == true){
-                    setState((){});
-                  }
-                },
-                child: Card(
-                  elevation: 2,
-                  margin: const EdgeInsets.only(bottom: 10),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                  child: ListTile(
-                    leading: const Icon(Icons.monetization_on, color: Colors.blue),
-                    title: Text(
-                      "${item.category} - \$${item.amount}",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      )
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.blue,
                     ),
-                    subtitle: Text(
-                        "${item.date} (${item.account})" + (item.reason.isNotEmpty ? "\n" + item.reason : ""),
-                    ),
-                    trailing: ElevatedButton(
-                      onPressed: (){
-                        dbHelper.changeImportance(item.id!);
-                        setState((){});
-                      },
-                      child: Icon(
-                        item.isHighlighted == 1 ? Icons.star : Icons.star_border,
-                        color: item.isHighlighted == 1 ? Colors.yellow[800] : Colors.grey,
-                      ),
-                    ),
-
-                    isThreeLine: item.reason.isNotEmpty,
-
                   ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[200],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child:
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(40, 5, 40, 5),
+                        child: Text(
+                            _viewingDate.month.toString().padLeft(2, '0') + "/" + _viewingDate.year.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      )
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      _nextMonth();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[100],
+                    ),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ]
+              )
+            ),
+            Expanded(
+              child:
+                FutureBuilder<List<TransactionModel>>(
+                  future: dbHelper.getTransactions(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text("Error: ${snapshot.error}"));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return _buildEmptyState();
+                    }
+
+                    final transactions = (snapshot.data!).where((item){
+                        try{
+                          final itemDate = DateTime.parse(item.date);
+                          return itemDate.month == _viewingDate.month && itemDate.year == _viewingDate.year;
+                        } catch (e) {
+                          return false;
+                        }
+                    }).toList();
+
+                    if(transactions.isEmpty){
+                      return _buildEmptyState();
+                    }
+
+                    return ListView.builder(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+                      itemCount: transactions.length,
+                      itemBuilder: (context, index) {
+                        final item = transactions[index];
+                        return GestureDetector(
+                          onTapDown: _getTabPosition,
+                          onLongPress: () => _showContextMenu(context, item.id!),
+                          onDoubleTap: () => _showContextMenu(context, item.id!),
+                          onTap: () async {
+                            final result = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PurchaseInfo(transaction: item),
+                              ),
+                            );
+
+                            if(result == true){
+                              setState((){});
+                            }
+                          },
+                          child: Card(
+                            elevation: 2,
+                            margin: const EdgeInsets.only(bottom: 10),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                            child: ListTile(
+                              leading: const Icon(Icons.monetization_on, color: Colors.blue),
+                              title: Text(
+                                "${item.category} - \$${item.amount}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )
+                              ),
+                              subtitle: Text(
+                                  "${item.date} (${item.account})" + (item.reason.isNotEmpty ? "\n" + item.reason : ""),
+                              ),
+                              trailing: ElevatedButton(
+                                onPressed: (){
+                                  dbHelper.changeImportance(item.id!);
+                                  setState((){});
+                                },
+                                child: Icon(
+                                  item.isHighlighted == 1 ? Icons.star : Icons.star_border,
+                                  color: item.isHighlighted == 1 ? Colors.yellow[800] : Colors.grey,
+                                ),
+                              ),
+
+                              isThreeLine: item.reason.isNotEmpty,
+
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
                 ),
-              );
-            },
-          );
-        },
+              ),
+          ]
+        ),
       ),
     );
   }
